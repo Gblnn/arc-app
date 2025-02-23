8cf8f2a854783fc5b355cc899fa14a3a8867ba56
Gokul <122973682+Gblnn@users.noreply.github.com>
2025-02-23 11:15:50 +0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,27 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 import Index from "./pages";
-import Timesheets from "./pages/timesheets";
 import AdminPage from "./pages/admin-page";
+import Invoice from "./pages/invoice";
 import Login from "./pages/login";
 import Profile from "./pages/profile";
-import Invoice from "./pages/invoice";
-import ErrorBoundary from "./components/ErrorBoundary";
+import Timesheets from "./pages/timesheets";
 import UserReset from "./pages/user-reset";
 import Users from "./pages/users";
 
 function App() {
   return (
     <Router>
-      <ErrorBoundary>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/user-reset" element={<UserReset />} />
-          <Route path="/index" element={<Index />} />
-          <Route path="/timesheets" element={<Timesheets />} />
-          <Route path="/admin" element={<AdminPage />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="profile/*" element={<Profile />} />
-          <Route path="/invoice-gen" element={<Invoice />} />
-        </Routes>
-      </ErrorBoundary>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/user-reset" element={<UserReset />} />
+        <Route path="/index" element={<Index />} />
+        <Route path="/timesheets" element={<Timesheets />} />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="profile/*" element={<Profile />} />
+        <Route path="/invoice-gen" element={<Invoice />} />
+      </Routes>
     </Router>
   );
 }
