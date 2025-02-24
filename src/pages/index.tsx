e3035d85a4a86b8d7df2c7fbff9d0dc34449d268
Gokul <122973682+Gblnn@users.noreply.github.com>
2025-02-24 12:54:04 +0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -130,7 +130,7 @@ export default function Index() {
         <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }}>
           <Back
             title="Arc"
-            subtitle={"v1.2"}
+            subtitle={"v2"}
             icon={<img src="/arc-logo.png" style={{ width: "2rem" }} />}
             noback
             extra={
