466bb146ebe973bbe7e06c6afa070ef88a12ea19
Gokul <122973682+Gblnn@users.noreply.github.com>
2025-02-24 11:02:00 +0400

    Update records.tsx

--- src/pages/records.tsx
+++ src/pages/records.tsx
@@ -1,225 +1,143 @@
 import { db } from "@/firebase";
-import {
-  collection,
-  getAggregateFromServer,
-  getDocs,
-  orderBy,
-  query,
-  sum,
-  where,
-} from "firebase/firestore";
+import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
 import { motion } from "framer-motion";
 import { LoaderCircle } from "lucide-react";
 import moment from "moment";
 import { useEffect, useState } from "react";
 
-const months = moment.months(); // Get an array of month names
+interface Record {
+  id: string;
+  start: { toDate: () => Date };
+  end: { toDate: () => Date } | "";
+}
 
 export default function Records() {
   const [loading, setLoading] = useState(false);
-  const [records, setRecords] = useState<any>([]);
-  const [totalSum, setSum] = useState(0);
-  const [totalOvertime, setOvertime] = useState(0);
-  const [selectedMonth, setSelectedMonth] = useState(moment().format("MMMM")); // Default to current month
+  const [records, setRecords] = useState<Record[]>([]);
+  const [selectedMonth] = useState(moment().format("MMMM")); // Default to current month
 
   useEffect(() => {
     fetchRecords();
-    getSum();
-    // console.log(records);
   }, [selectedMonth]); // Fetch records when the selected month changes
 
   const fetchRecords = async () => {
     setLoading(true);
-    const RecordCollection = collection(db, "records");
-    const recordQuery = query(
-      RecordCollection,
-      where("email", "==", window.name),
-      orderBy("start", "desc"),
-      where("start", "==", moment(selectedMonth).format("MMMM"))
-    );
-    const querySnapshot = await getDocs(recordQuery);
-    setLoading(false);
-    const fetchedData: any = [];
+    try {
+      const RecordCollection = collection(db, "records");
+      const recordQuery = query(
+        RecordCollection,
+        where("email", "==", window.name),
+        orderBy("start", "desc"),
+        where("start", "==", moment(selectedMonth).format("MMMM"))
+      );
+      const querySnapshot = await getDocs(recordQuery);
+      const fetchedData = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      })) as Record[];
 
-    querySnapshot.forEach((doc: any) => {
-      fetchedData.push({ id: doc.id, ...doc.data() });
-    });
-    setRecords(fetchedData);
+      setRecords(fetchedData);
+    } catch (error) {
+      console.error("Error fetching records:", error);
+    } finally {
+      setLoading(false);
+    }
   };
 
-  const getSum = async () => {
-    const recordQuery = query(
-      collection(db, "records"),
-      where("email", "==", window.name),
-      orderBy("start", "desc")
+  const calculateHours = (start: Date, end: Date) => {
+    return Math.round(
+      moment.duration(moment(end).diff(moment(start))).get("hours")
     );
-    const snapshot = await getAggregateFromServer(recordQuery, {
-      total: sum("total"),
-      overtime: sum("overtime"),
-    });
-    // console.log("total hours", snapshot.data().total);
-    setSum(snapshot.data().total);
-    setOvertime(snapshot.data().overtime);
+  };
+
+  const calculateOvertime = (hours: number) => {
+    return hours > 10 ? hours - 10 : 0;
   };
 
   return (
-    <>
-      <div
-        style={{
-          width: "100%",
-          border: "",
-          height: "70svh",
-          padding: "",
-          display: "flex",
-          flexFlow: "column",
-        }}
-      >
-        {!loading ? (
-          <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }}>
-            <div
-              style={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                marginBottom: "1rem",
-              }}
-            >
-              {/* <CalendarDays color="crimson" />
-              <CustomDropdown
-                value={selectedMonth}
-                onChange={(month) => setSelectedMonth(month)}
-                options={months.map((month) => ({
-                  value: month,
-                  label: month,
-                }))}
-                placeholder="Select a month"
-              /> */}
-            </div>
-            <p
-              style={{
-                fontWeight: "500",
-                fontSize: "1.5rem",
-                display: "flex",
-                alignItems: "center",
-                gap: "0.5rem",
-                margin: "",
-                border: "",
-                justifyContent: "center",
-              }}
-            >
-              {selectedMonth}{" "}
-              <b style={{ color: "crimson", fontWeight: "800" }}>
-                {moment().format("YYYY")}
-              </b>
-            </p>
-            <br />
-            {/* <div
-              style={{
-                border: "",
-                display: "flex",
-                justifyContent: "space-between",
-                fontSize: "0.9rem",
-              }}
-            >
-              <button className="wide-btn">
-                Hours <b style={{ color: "" }}>{totalSum}</b>
-              </button>
-              
-              <button className="wide-btn">
-                Overtime <b style={{ color: "lightgreen" }}>{totalOvertime}</b>
-              </button>
-            </div> */}
-            <br />
-            <table style={{ width: "100%", fontSize: "0.9rem" }}>
-              <thead style={{}}>
-                <tr style={{ background: "rgba(100 100 100/ 20%)" }}>
-                  <th>Date</th>
-                  <th>Start</th>
-                  <th>End</th>
-                  <th>Total</th>
-                  <th style={{}}>OT</th>
-                </tr>
-              </thead>
-
-              <tbody
-                style={{
-                  textAlign: "center",
-                  background: "rgba(100 100 100/ 10%)",
-                }}
-              >
-                {records.map((e: any) => (
-                  <tr key={e.id}>
-                    <td>{moment(e.start.toDate()).format("DD/MM/YY")}</td>
-                    <td>
-                      {e.start
-                        ? e.start && moment(e.start.toDate()).format("hh:mm A")
-                        : "-"}
-                    </td>
-                    <td>
-                      {e.end != ""
-                        ? moment(e.end.toDate()).format("hh:mm A")
-                        : "-"}
-                    </td>
-                    <td>
-                      {/* {e.end
-                        ? Number(moment(e.end.toDate()).format("hh")) -
-                          Number(moment(e.start.toDate()).format("hh"))
-                        : "-"} */}
-                      {e.end
-                        ? Math.round(
-                            moment
-                              .duration(
-                                moment(e.end.toDate()).diff(
-                                  moment(e.start.toDate())
-                                )
-                              )
-                              .get("hours")
-                          )
-                        : "-"}
-                    </td>
-                    <td>
-                      {e.end &&
-                      moment
-                        .duration(
-                          moment(e.end.toDate()).diff(moment(e.start.toDate()))
-                        )
-                        .get("hours") > 10
-                        ? Math.round(
-                            moment
-                              .duration(
-                                moment(e.end.toDate()).diff(
-                                  moment(e.start.toDate())
-                                )
-                              )
-                              .get("hours") - 10
-                          )
-                        : "-"}
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-            <br />
-          </motion.div>
-        ) : (
-          <div
+    <div
+      style={{
+        width: "100%",
+        height: "70svh",
+        display: "flex",
+        flexFlow: "column",
+      }}
+    >
+      {!loading ? (
+        <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }}>
+          <p
             style={{
-              width: "100%",
+              fontWeight: "500",
+              fontSize: "1.5rem",
               display: "flex",
-              height: "70svh",
-              justifyContent: "center",
               alignItems: "center",
+              gap: "0.5rem",
+              justifyContent: "center",
             }}
           >
-            <LoaderCircle
-              color="crimson"
-              height={"3rem"}
-              width={"3rem"}
-              className="animate-spin"
-            />
-          </div>
-        )}
-      </div>
-    </>
+            {selectedMonth}{" "}
+            <b style={{ color: "crimson", fontWeight: "800" }}>
+              {moment().format("YYYY")}
+            </b>
+          </p>
+          <br />
+          <br />
+          <table style={{ width: "100%", fontSize: "0.9rem" }}>
+            <thead>
+              <tr style={{ background: "rgba(100 100 100/ 20%)" }}>
+                <th>Date</th>
+                <th>Start</th>
+                <th>End</th>
+                <th>Total</th>
+                <th>OT</th>
+              </tr>
+            </thead>
+
+            <tbody
+              style={{
+                textAlign: "center",
+                background: "rgba(100 100 100/ 10%)",
+              }}
+            >
+              {records.map((record) => {
+                const startDate = record.start.toDate();
+                const endDate = record.end ? record.end.toDate() : null;
+                const totalHours = endDate
+                  ? calculateHours(startDate, endDate)
+                  : 0;
+
+                return (
+                  <tr key={record.id}>
+                    <td>{moment(startDate).format("DD/MM/YY")}</td>
+                    <td>{moment(startDate).format("hh:mm A")}</td>
+                    <td>{endDate ? moment(endDate).format("hh:mm A") : "-"}</td>
+                    <td>{endDate ? totalHours : "-"}</td>
+                    <td>{endDate ? calculateOvertime(totalHours) : "-"}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+          <br />
+        </motion.div>
+      ) : (
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            height: "70svh",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <LoaderCircle
+            color="crimson"
+            height={"3rem"}
+            width={"3rem"}
+            className="animate-spin"
+          />
+        </div>
+      )}
+    </div>
   );
 }
